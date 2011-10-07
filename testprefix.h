95caf2ed7929b3066d4072aa585b3da57f0d8a11
Kyle J. McKay <mackyle@gmail.com>
2011-10-07 16:57:25 -0700

    Add mingw checktests compatibility

--- testprefix.h
+++ testprefix.h
@@ -1,6 +1,10 @@
 #undef __APPLE_CC__
 #define __APPLE_CC__ 5627
+#include <stdlib.h>
 #include <stdio.h>
-#ifndef _WIN32
+#ifdef _WIN32
+#define random() rand()
+#else /* !_WIN32 */
+#include <unistd.h>
 #include <sys/wait.h>
-#endif
+#endif /* !_WIN32 */
