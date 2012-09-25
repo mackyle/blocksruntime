140fbdf93d152a5b6b5c3756cb7584cad4bb2da0
Kyle J. McKay <mackyle@gmail.com>
2012-09-25 13:33:11 -0700

    testprefix.h: Work around unfortunate Linux unistd.h __block incompatibility

--- testprefix.h
+++ testprefix.h
@@ -5,6 +5,12 @@
 #ifdef _WIN32
 #define random() rand()
 #else /* !_WIN32 */
+#ifdef __linux__
+#undef __block
+#endif
 #include <unistd.h>
+#ifdef __linux__
+#define __block __attribute__((__blocks__(byref)))
+#endif
 #include <sys/wait.h>
 #endif /* !_WIN32 */
